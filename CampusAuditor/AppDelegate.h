1dfdac57034c544620c05800a6fb4c9a00716862
Manish Rawat <himshwetasinha@Himshwetas-iMac.local>
2014-07-10 12:07:13 +0530

    Initial Commit

---
+++ CampusAuditor/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  CampusAuditor
+//
+//  Created by Manish Rawat on 10/07/14.
+//  Copyright (c) 2014 Aequor Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CampusAuditor/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CampusAuditor
+//
+//  Created by Manish Rawat on 10/07/14.
+//  Copyright (c) 2014 Aequor Technologies Pvt Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
